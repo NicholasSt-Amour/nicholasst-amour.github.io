895705edc3e225b2f69c72f692b8b1c84342fcf3
NicholasSt-Amour <nicholas.m.stamour@gmail.com>
2025-08-03 15:37:23 -0400

    Projects page completed

--- js/footer.js
+++ js/footer.js
@@ -1,4 +1,4 @@
-// Format: July 13, 2025
-const options = { year: 'numeric', month: 'long', day: 'numeric' };
-const updated = new Date(document.lastModified).toLocaleDateString(undefined, options);
+// Format: July 13, 2025, 15:58
+const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
+const updated = new Date(document.lastModified).toLocaleString(undefined, options);
 document.getElementById('last-updated').textContent = `© 2025 Nicholas St-Amour. Last updated ${updated}.`;
